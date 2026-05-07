db795032f589dea9bb5df7dcd604dc2a36ae7536
gracemmanuel <geeemmanuel1702@gmail.com>
2026-05-08 00:25:34 +0300

    owner start

--- bodaboda_be/mainschema.py
+++ bodaboda_be/mainschema.py
@@ -1,11 +1,12 @@
 import graphene
 from bodaboda_auth.schema import Query as AuthQuery, Mutation as AuthMutation
 from rides.schema import Query as RidesQuery, Mutation as RidesMutation
+from fleet.schema import Query as FleetQuery, Mutation as FleetMutation
 
-class Query(AuthQuery, RidesQuery, graphene.ObjectType):
+class Query(AuthQuery, RidesQuery, FleetQuery, graphene.ObjectType):
     pass
 
-class Mutation(AuthMutation, RidesMutation, graphene.ObjectType):
+class Mutation(AuthMutation, RidesMutation, FleetMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
